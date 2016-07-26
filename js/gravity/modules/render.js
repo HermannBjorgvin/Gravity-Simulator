771ca888e2480248f5fadf02e3b6e28bc2e66269
MuhsinFatih <muhsinfatih@gmail.com>
2016-07-26 04:04:54 +0300

    prettified and fixed keyboard controls
    I was wandering around your code and saw the keyboard control code and
    realized it's not working. Made it work and couldn't stop myself from
    making it look cool! :) use W A S D for movement and Q E for zoom and R
    for reset camera. I made some transition effect which I am sure you will
    like :thumbsup: .
    I also added "draw object path" toggle since I was annoyed by the people
    around me not understanding that the moon on the app actually does a
    pretty nice orbit around earth. I think removing the path line being an
    option helps better visualize the thing.
    
    One last thing: I couldn't exactly figure out how the renderGrid
    function works so I couldn't touch it so I had to set the grid toggle to
    unchecked by default (which if you ask me looks better :smile:
    especially with the zoom in at startup effect). What happens is that
    when I zoom in or out, grid shifts like crazy and disturbs my eyes. I
    hope you can rewrite that function later on.

--- js/gravity/modules/render.js
+++ js/gravity/modules/render.js
@@ -18,7 +18,17 @@ define(['jquery', 'underscore'], function($, _){
 	var camera = {
 		x:0,
 		y:0,
-		zoom: 1,
+		marginX:0,
+		marginY:0,
+        marginZoom:0,
+		preferredX:0,
+		preferredY:0,
+		preferredZoom:0,
+		drag:50,
+		xIT:0,
+		yIT:0,
+        zoomIT:0,
+		zoom: 0,
 		locked: true,
 		getX: function(p_x){
 			var x = (p_x*this.zoom - this.x*this.zoom);
@@ -50,73 +60,128 @@ define(['jquery', 'underscore'], function($, _){
 		ctx.beginPath();
 	}
 
+	var zoomInput = document.getElementById('menu-zoom');
 	function moveCamera(e){
-		switch(String.fromCharCode(e.charCode)){
+        
+		switch(String.fromCharCode(e.charCode).toLowerCase()){
 			case 'w':
-				camera.y -= 10;
+				camera.preferredY -= camera.drag;
+				camera.yIT = fps;
 				break;
 			case 'a':
-				camera.x -= 10;
+				camera.preferredX -= camera.drag;
+				camera.xIT = fps;
 				break;
 			case 's':
-				camera.y += 10;
+				camera.preferredY += camera.drag;
+				camera.yIT = fps;
 				break;
 			case 'd':
-				camera.x += 10;
+				camera.preferredX += camera.drag;
+				camera.xIT = fps;
 				break;
+		    case 'e':
+		        camera.preferredZoom = Math.round((camera.preferredZoom + 0.1) * 10) / 10;
+		        zoomInput.value = camera.preferredZoom;
+		        camera.zoomIT = fps;
+		        break;
+		    case 'q':
+		        if (camera.preferredZoom > 0) {
+		            camera.preferredZoom = Math.round((camera.preferredZoom - 0.1) * 10) / 10;
+		            zoomInput.value = camera.preferredZoom;
+		            camera.zoomIT = fps;
+		        }
+		        break;
+		    case 'r':
+		        api.resetCamera();
+		        break;
 		}
 	}
 
+
+	var initialMarginX = 0, initialMarginY = 0, initialMarginZoom = 0;
+	var offsetX = 0, offsetY = 0, offsetZoom = 0;
 	function centerCamera(){
 		var l_spacetime = spacetime.getSpace();
 		for (var i = l_spacetime.length - 1; i >= 0; i--) {
 			var object = l_spacetime[i]
 			if (object.cameraFocus == true) {
-				camera.x = object.x - canvas.width/2/camera.zoom;
-				camera.y = object.y - canvas.height/2/camera.zoom;
+				if(camera.xIT > 0) {
+				    if (camera.xIT == fps) {
+				        offsetX = camera.preferredX - camera.marginX;
+				        initialMarginX = camera.marginX;
+				    }
+					camera.xIT -= 1;
+					camera.marginX = initialMarginX + Math.sign(offsetX) * Math.sqrt(Math.pow(offsetX,2) - Math.pow(offsetX * (fps - camera.xIT)/fps - offsetX,2));
+				}
+				else camera.marginX = camera.preferredX;
+
+				if (camera.yIT > 0) {
+				    if (camera.yIT == fps) {
+				        offsetY = camera.preferredY - camera.marginY;
+				        initialMarginY = camera.marginY;
+				    }
+					camera.yIT -= 1;
+					camera.marginY = initialMarginY + Math.sign(offsetY) * Math.sqrt(Math.pow(offsetY,2) - Math.pow(offsetY * (fps - camera.yIT)/fps - offsetY,2));
+				}
+				else camera.marginY = camera.preferredY;
+				
+				if (camera.zoomIT > 0) {
+				    if (camera.zoomIT == fps) {
+				        offsetZoom = camera.preferredZoom - camera.zoom;
+				        initialMarginZoom = camera.zoom;
+				    }
+
+				    camera.zoomIT -= 1;
+				    camera.zoom = initialMarginZoom + Math.sign(offsetZoom) * Math.sqrt(Math.pow(offsetZoom, 2) - Math.pow(offsetZoom * (fps - camera.zoomIT)/fps - offsetZoom, 2));
+				} else camera.zoom = camera.preferredZoom;
+
+				camera.x = object.x - canvas.width / 2 / camera.zoom + camera.marginX;
+				camera.y = object.y - canvas.height / 2 / camera.zoom + camera.marginY;
 			};
 		};
 	}
-
+	var menuDrawPath = false;
+    
 	function renderObject(object){
 		// --------------------
 		// | Draw object path |
-		// --------------------
-		(function(){
-			if (object.path.length > 3)
-			{
-				ctx.beginPath();
-				ctx.moveTo(
-					camera.getX(object.path[0].x),
-					camera.getY(object.path[0].y)
-				);
+	    // --------------------
+	    if (menuDrawPath) {
+	        (function () {
+	            if (object.path.length > 3) {
+	                ctx.beginPath();
+	                ctx.moveTo(
+                        camera.getX(object.path[0].x),
+                        camera.getY(object.path[0].y)
+                    );
 
-				for (i = 1; i < object.path.length - 2; i ++)
-				{
-					var xc = (object.path[i].x + object.path[i + 1].x) / 2;
-					var yc = (object.path[i].y + object.path[i + 1].y) / 2;
+	                for (i = 1; i < object.path.length - 2; i++) {
+	                    var xc = (object.path[i].x + object.path[i + 1].x) / 2;
+	                    var yc = (object.path[i].y + object.path[i + 1].y) / 2;
 
-					ctx.quadraticCurveTo(
-						camera.getX(object.path[i].x),
-						camera.getY(object.path[i].y),
-						camera.getX(xc),
-						camera.getY(yc)
-					);
-				}
-				
-				// curve through the last two points
-				ctx.quadraticCurveTo(
-					camera.getX(object.path[object.path.length-2].x),
-					camera.getY(object.path[object.path.length-2].y),
-					camera.getX(object.path[object.path.length-1].x),
-					camera.getY(object.path[object.path.length-1].y)
-				);
+	                    ctx.quadraticCurveTo(
+                            camera.getX(object.path[i].x),
+                            camera.getY(object.path[i].y),
+                            camera.getX(xc),
+                            camera.getY(yc)
+                        );
+	                }
 
-				ctx.lineWidth = 1;
-				ctx.strokeStyle = "#666";
-				ctx.stroke();
-			};
-		})();
+	                // curve through the last two points
+	                ctx.quadraticCurveTo(
+                        camera.getX(object.path[object.path.length - 2].x),
+                        camera.getY(object.path[object.path.length - 2].y),
+                        camera.getX(object.path[object.path.length - 1].x),
+                        camera.getY(object.path[object.path.length - 1].y)
+                    );
+
+	                ctx.lineWidth = 1;
+	                ctx.strokeStyle = "#666";
+	                ctx.stroke();
+	            };
+	        })();
+	    }
 
 		// ---------------
 		// | Draw object |
@@ -248,16 +313,17 @@ define(['jquery', 'underscore'], function($, _){
 		clearInterval(renderLoop);
 	}
 
-	api.toggleGrid = function(){
-		settings.showGrid = !settings.showGrid;
+	api.setDrawGrid = function(value){
+		settings.showGrid = value;
 	}
 
 	api.updateMassMultiplier = function(p_massMultiplier){
 		massMultiplier = p_massMultiplier;
 	}
 
-	api.changeZoom = function(p_zoom){
-		camera.zoom = p_zoom;
+	api.changeZoom = function (p_zoom) {
+	    camera.preferredZoom = parseFloat(p_zoom);
+	    camera.zoomIT = fps;
 	}
 	
 	api.setMouse = function(p_mouse){
@@ -266,6 +332,22 @@ define(['jquery', 'underscore'], function($, _){
 
 	api.getCamera = function(){
 		return camera
+	}
+
+	api.resetCamera = function () {
+	    camera.preferredX = 0;
+	    camera.preferredY = 0;
+	    camera.preferredZoom = 1;
+	    zoomInput.value = 1;
+
+
+	    camera.xIT = fps;
+	    camera.yIT = fps;
+	    camera.zoomIT = fps;
+	}
+
+	api.setDrawPath = function (value) {
+	    menuDrawPath = value;
 	}
 
 	return api;
